2c53e5c5fd59ad70bba67e379035dd5f0b26685d
QuangHieu1011 <165991638+QuangHieu1011@users.noreply.github.com>
2025-11-15 19:36:57 +0700

    Update Paginated in Product

--- backend/src/services/ProductService.js
+++ backend/src/services/ProductService.js
@@ -86,18 +86,22 @@ const deleteProduct = (id) => {
         }
     });
 };
-const getAllProduct = () => {
+const getAllProduct = (limit = 8, page = 0) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const allProduct = await Product.find()
+            const totalProduct = await Product.countDocuments()
+            const allProduct = await Product.find().limit(limit).skip(limit * page)
             resolve({
                 status: 'OK',
                 message: 'Get all product success',
-                data: allProduct
+                data: allProduct,
+                total: totalProduct,
+                pageCurrent: Number(page) + 1,
+                totalPage: Math.ceil(totalProduct / limit)
             });
         } catch (e) {
              reject(e);
-        }
+        } 
     });
 };
 const getDetailsProduct = (id) => {
